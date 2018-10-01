ed6b999156c830ab9f8082f1f2f336da3a8a9076
nikhil <nikhilreddy118333@gmail.com>
2018-10-01 18:08:45 +0530

    left del ed

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App1 from './App';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import 'bootstrap/dist/css/bootstrap.css'
-
-ReactDOM.render(<App1 />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
